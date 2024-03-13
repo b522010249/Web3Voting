5ee31dc2431120839c8896b9f7b890639af91459
Nonz7 <nonscbza555@gmail.com>
2024-03-14 00:44:23 +0700

    first commit non

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,116 @@
-// Footer.js
-import React from 'react';
-import '../css/Footer.css'; // Create this CSS file for styling if needed
+import React from 'react'
+import { Button } from './Button'
+import './Footer.css'
+import { Link } from 'react-router-dom/cjs/react-router-dom.min'
 
-const Footer = () => {
+function Footer() {
   return (
-    <footer className="footer">
-      <p>&copy; 2024 Web3 Voting App</p>
-    </footer>
-  );
-};
+    <div className='footer-container'>
+      <section className='footer-subscription'>
+        <p className='footer-subscription-heading'>
+            Thank you so much!
 
-export default Footer;
+        </p>
+        <p className='footer-subscription-text'>
+            Feedback.
+        </p>
+        <div className='input-areas'>
+            <form>
+                <input type='email' name='email' 
+                placeholder='Your Message' className='footer-input'/>
+
+                <Button buttonStyle='btn--outline'>Send</Button>
+            </form>
+        </div>
+      </section>
+      {/*<div className='footer-links'>
+        <div className='footer-link-wrapper'>
+            <div className='footer-link-items'>
+                <h2>About Us</h2>
+                <Link to='/sign-up'>How it works</Link>
+                <Link to='/'>Testimonials</Link>
+                <Link to='/'>Careers</Link>
+                <Link to='/'>Investors</Link>
+                <Link to='/'>Term of Service</Link>
+            </div>
+            <div className='footer-link-items'>
+                <h2>Contact Us</h2>
+                <Link to='/'>Contact</Link>
+                <Link to='/'>Support</Link>
+                <Link to='/'>Destinations</Link>
+                <Link to='/'>Sponsorships</Link>
+            </div>
+        </div>
+        <div className='footer-link-wrapper'>
+            <div className='footer-link-items'>
+                <h2>Videos</h2>
+                <Link to='/'>Submit Video</Link>
+                <Link to='/'>Ambassadors</Link>
+                <Link to='/'>Agency</Link>
+                <Link to='/'>Influencer</Link>
+            </div>
+            <div className='footer-link-items'>
+                <h2>Social Media</h2>
+                <Link to='/'>Instagram</Link>
+                <Link to='/'>Facebook</Link>
+                <Link to='/'>Youtube</Link>
+                <Link to='/'>Twitter</Link>
+            </div>
+        </div>
+  </div>*/}
+      <section className='social-media'>
+        <div className='social-media-wrap'>
+            <div className='footer-logo'>
+                <Link to='/' className='social-logo'>
+                VSC <i className='fab fa-typo3'></i>
+                </Link>
+            </div>
+            <small className='website-rights'>VSC © 2024</small>
+            <div className='social-icons'>
+                <Link className='social-icon-link facebook'
+                to='/'
+                target='_bank'
+                aria-label='FaceBook'
+                >
+                    <i className='fab fa-facebook-f'></i>
+                </Link>
+                <Link className='social-icon-link instagram'
+                to='/'
+                target='_bank'
+                aria-label='Instagram'
+                >
+                    <i className='fab fa-instagram'></i>
+                </Link>
+                <Link
+              class='social-icon-link youtube'
+              to='/'
+              target='_blank'
+              aria-label='Youtube'
+            >
+              <i class='fab fa-youtube' />
+            </Link>
+            <Link
+              class='social-icon-link twitter'
+              to='/'
+              target='_blank'
+              aria-label='Twitter'
+            >
+              <i class='fab fa-twitter' />
+            </Link>
+            <Link
+              class='social-icon-link linkedin'
+              to='/'
+              target='_blank'
+              aria-label='LinkedIn'
+            >
+              <i class='fab fa-linkedin' />
+            </Link>
+            </div>
+        </div>
+
+      </section>
+    </div>
+  )
+}
+
+export default Footer
